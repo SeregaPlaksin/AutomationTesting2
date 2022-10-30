380602207ea37c12f5bd0ba0dd8d40f9ec7f5691
Plaksin Sergey <SPlaksin@bellintegrator.com>
2022-10-30 15:20:59 +0300

    secret file decrypted

--- pom/index_page.py
+++ pom/index_page.py
@@ -1,21 +1,33 @@
 from base_object.base import BaseObject
+from base_object.helper import Helper
 from base_object.locators import Locators
+import  json
+import os
+
+
+path = os.path.dirname(os.path.realpath(__file__))
+path2 = str(path)+'\\passwords.json'
+file = open(path2, 'rb')
+my_passwords = json.load(file)
+password = my_passwords['password']
+username = my_passwords['username']
+
 
 
 class IndexPage(BaseObject):
     pass
 
 
-class AuthPage(BaseObject):
+class AuthPage(BaseObject, Helper):
 
     def enter_correct_username(self):
-        self.to_input('css', Locators.LOGIN, 'standard_user')
+        self.to_input('css', Locators.LOGIN, username)
 
     def enter_uncorrect_username(self):
         self.to_input('css', Locators.LOGIN, '123qwe')
 
     def enter_correct_password(self):
-        self.to_input('css', Locators.PASS, 'secret_sauce')
+        self.to_input('css', Locators.PASS, password)
 
     def click_login_button(self):
         self.to_click('css', Locators.LOGIN_BUTTON)
